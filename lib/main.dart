fe6e30a5158783f3609356c908721b3403e9c64a
mambaur <alwiroziq1998@gmail.com>
2022-11-14 01:09:35 +0700

    add user model

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter')),
+    );
   }
 }
---
+++ lib/user.dart
@@ -0,0 +1,10 @@
+class User {
+  int? id;
+  String? name;
+
+  User({this.id, this.name});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(id: json['id'], name: json['name']);
+  }
+}
